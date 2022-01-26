082b84608e7c7101e14dc54fbdabe6dd88ac19da
shashi kumar <shashi16ic14@gmail.com>
2022-01-26 22:33:31 +0530

    link add

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -15,8 +15,7 @@ export const projectsData = [
     projectDesc:
       " Upgrad is an Edtech platform, We built this website using HTML | JAVASCRIPT | CSS, for team colloboration we used github regularly, we were 3 member in the team, indivisual has given there task and within the 4 days we deployed some main functionality like login,signup,coursecheck page etc. This was my first ever project, i really learned a lot from this.",
     tags: ["HTML", "CSS", "javaScript"],
-    code: "https://github.com/tonyPooyappallil/upgradCloneProject.git",
-    demo: "https://drive.google.com/file/d/11Ym3d68SAWb59MunFhrLPtKXyzoeEfYa/view?usp=drivesdk",
+    code: "https://balram1996.github.io/",
     image: "https://miro.medium.com/max/1400/1*g9XiJfAHoz11S83k54JAHA.png",
   },
   {
